cb484603654fa687ecc90e4d7cdf3bfb43c6ac69
Peter Bjorklund <piot@hotmail.com>
2020-12-30 13:31:25 +0100

    chore: fix minor bugs

--- src/lib/circular_buffer.c
+++ src/lib/circular_buffer.c
@@ -1,120 +1,101 @@
+#include <clog/clog.h>
 #include <discoid/circular_buffer.h>
 #include <tiny-libc/tiny_libc.h>
-#include <clog/clog.h>
 
-void discoidBufferInit(DiscoidBuffer *self, size_t maxSize)
+void discoidBufferInit(DiscoidBuffer* self, size_t maxSize)
 {
-	self->buffer = tc_malloc_type_count(uint8_t, maxSize);
-	self->writeIndex = 0;
-	self->readIndex = 0;
-	self->capacity = maxSize;
-	self->size = 0;
+    self->buffer = tc_malloc_type_count(uint8_t, maxSize);
+    self->writeIndex = 0;
+    self->readIndex = 0;
+    self->capacity = maxSize;
+    self->size = 0;
 }
 
-void discoidBufferDestroy(DiscoidBuffer *self)
+void discoidBufferDestroy(DiscoidBuffer* self)
 {
-	tc_free(self->buffer);
+    tc_free(self->buffer);
 }
 
-int discoidBufferReadAvailable(const DiscoidBuffer *self)
+int discoidBufferReadAvailable(const DiscoidBuffer* self)
 {
-	return self->size;
+    return self->size;
 }
 
-int discoidBufferWrite(DiscoidBuffer *self, const uint8_t *data, size_t sampleCountInTarget)
+int discoidBufferWrite(DiscoidBuffer* self, const uint8_t* data, size_t sampleCountInTarget)
 {
-	const uint8_t *source = data;
-	int sampleCount = sampleCountInTarget;
+    const uint8_t* source = data;
+    int sampleCount = sampleCountInTarget;
 
-	int availableWriteCount = self->capacity - self->size;
-	if (sampleCount > availableWriteCount)
-	{
-		return -2;
-	}
-
-	if (sampleCount == 0)
-	{
-		return 0;
-	}
-
-	int firstAvailable = self->capacity - self->writeIndex;
-	int firstRun = sampleCount;
-	if (firstRun > firstAvailable)
-	{
-		firstRun = firstAvailable;
-	}
-	tc_memcpy_type(uint8_t, self->buffer + self->writeIndex, source, firstRun);
-
-	sampleCount -= firstRun;
-	source += firstRun;
-	self->size += firstRun;
-	self->writeIndex += firstRun;
-	self->writeIndex %= self->capacity;
-	if (sampleCount == 0)
-	{
-		return 0;
-	}
-
-	tc_memcpy_type(uint8_t, self->buffer + self->writeIndex, source, sampleCount);
-	self->writeIndex += sampleCount;
-	self->writeIndex %= self->capacity;
-	self->size += sampleCount;
-
-	return 0;
+    int availableWriteCount = self->capacity - self->size;
+    if (sampleCount > availableWriteCount) {
+        CLOG_WARN("discoid buffer. Out of capacity. wanted to write %d but we are at (%zu / %zu)", sampleCount,
+                  self->size, self->capacity);
+        return -2;
+    }
+    if (sampleCount == 0) {
+        return 0;
+    }
+    int firstAvailable = self->capacity - self->writeIndex;
+    int firstRun = sampleCount;
+    if (firstRun > firstAvailable) {
+        firstRun = firstAvailable;
+    }
+    tc_memcpy_type(uint8_t, self->buffer + self->writeIndex, source, firstRun);
+    sampleCount -= firstRun;
+    source += firstRun;
+    self->size += firstRun;
+    self->writeIndex += firstRun;
+    self->writeIndex %= self->capacity;
+    if (sampleCount == 0) {
+        return 0;
+    }
+    tc_memcpy_type(uint8_t, self->buffer + self->writeIndex, source, sampleCount);
+    self->writeIndex += sampleCount;
+    self->writeIndex %= self->capacity;
+    self->size += sampleCount;
+    return 0;
 }
 
-int discoidBufferSkip(DiscoidBuffer *self, size_t octetCount)
+int discoidBufferSkip(DiscoidBuffer* self, size_t octetCount)
 {
-	if (octetCount > self->size)
-	{
-		return -1;
-	}
-	self->readIndex += octetCount;
-	self->readIndex %= self->capacity;
-	self->size -= octetCount;
-
-	return 0;
+    if (octetCount > self->size) {
+        return -1;
+    }
+    self->readIndex += octetCount;
+    self->readIndex %= self->capacity;
+    self->size -= octetCount;
+    return 0;
 }
 
-int discoidBufferPeek(const DiscoidBuffer *self, size_t readIndex, uint8_t *target, size_t readCount)
+int discoidBufferPeek(const DiscoidBuffer* self, size_t readIndex, uint8_t* target, size_t readCount)
 {
-	size_t availableFirstRun = self->capacity - readIndex;
-	size_t firstRun = readCount;
-	size_t secondRun = 0;
+    size_t availableFirstRun = self->capacity - readIndex;
+    size_t firstRun = readCount;
+    size_t secondRun = 0;
+    if (readCount > availableFirstRun) {
+        firstRun = availableFirstRun;
+        secondRun = readCount - availableFirstRun;
+    }
+    // CLOG_INFO("peeking at readIndex %zu. First run:%d second:%d", readIndex, firstRun, secondRun);
 
-	if (readCount > availableFirstRun)
-	{
-		firstRun = availableFirstRun;
-		secondRun = readCount - availableFirstRun;
-	}
-	//CLOG_INFO("peeking at readIndex %zu. First run:%d second:%d", readIndex, firstRun, secondRun);
-
-	tc_memcpy_type(uint8_t, target, self->buffer + readIndex, firstRun);
-	if (secondRun > 0)
-	{
-		tc_memcpy_type(uint8_t, target + firstRun, self->buffer, secondRun);
-	}
-	return secondRun;
+    tc_memcpy_type(uint8_t, target, self->buffer + readIndex, firstRun);
+    if (secondRun > 0) {
+        tc_memcpy_type(uint8_t, target + firstRun, self->buffer, secondRun);
+    }
+    return secondRun;
 }
 
-int discoidBufferRead(DiscoidBuffer *self, uint8_t *output, size_t requiredCount)
+int discoidBufferRead(DiscoidBuffer* self, uint8_t* output, size_t requiredCount)
 {
-	if (requiredCount > self->size)
-	{
-		return -2;
-	}
-
-	int secondRun = discoidBufferPeek(self, self->readIndex, output, requiredCount);
-
-	self->size -= requiredCount;
-	if (secondRun > 0)
-	{
-		self->readIndex = secondRun;
-	}
-	else
-	{
-		self->readIndex += requiredCount;
-	}
-
-	return 0;
+    if (requiredCount > self->size) {
+        return -2;
+    }
+    int secondRun = discoidBufferPeek(self, self->readIndex, output, requiredCount);
+    self->size -= requiredCount;
+    if (secondRun > 0) {
+        self->readIndex = secondRun;
+    } else {
+        self->readIndex += requiredCount;
+    }
+    return 0;
 }
