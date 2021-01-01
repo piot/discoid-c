3455329c1583230eba431707ade5f118c9c67d72
Peter Bjorklund <piot@hotmail.com>
2021-01-01 16:54:39 +0100

    style: format

--- src/include/discoid/circular_buffer.h
+++ src/include/discoid/circular_buffer.h
@@ -4,21 +4,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct DiscoidBuffer
-{
-	size_t capacity;
-	uint8_t *buffer;
-	size_t writeIndex;
-	size_t readIndex;
-	size_t size;
+typedef struct DiscoidBuffer {
+    size_t capacity;
+    uint8_t* buffer;
+    size_t writeIndex;
+    size_t readIndex;
+    size_t size;
 } DiscoidBuffer;
 
-void discoidBufferInit(DiscoidBuffer *self, size_t maxSize);
-void discoidBufferDestroy(DiscoidBuffer *self);
-int discoidBufferReadAvailable(const DiscoidBuffer *self);
-int discoidBufferSkip(DiscoidBuffer *self, size_t octetCount);
-int discoidBufferWrite(DiscoidBuffer *self, const uint8_t *data, size_t sampleCountInTarget);
-int discoidBufferRead(DiscoidBuffer *self, uint8_t *output, size_t requiredCount);
-int discoidBufferPeek(const DiscoidBuffer *self, size_t readIndex, uint8_t *target, size_t readCount);
+void discoidBufferInit(DiscoidBuffer* self, size_t maxSize);
+void discoidBufferDestroy(DiscoidBuffer* self);
+int discoidBufferReadAvailable(const DiscoidBuffer* self);
+int discoidBufferSkip(DiscoidBuffer* self, size_t octetCount);
+int discoidBufferWrite(DiscoidBuffer* self, const uint8_t* data, size_t sampleCountInTarget);
+int discoidBufferRead(DiscoidBuffer* self, uint8_t* output, size_t requiredCount);
+int discoidBufferPeek(const DiscoidBuffer* self, size_t readIndex, uint8_t* target, size_t readCount);
 
 #endif
